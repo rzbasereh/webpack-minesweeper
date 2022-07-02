a11245d10ff0d5b5320eb9d24859aab82b0decb5
Reza Basereh <rzbasereh1@gmail.com>
2022-07-03 01:41:53 +0430

    feat: config webpack css and scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
   mode: 'development',
   entry: './src/index.tsx',
+
+  plugins: [new MiniCssExtractPlugin()],
+
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: ['babel-loader', 'ts-loader'],
         exclude: /node_modules/,
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
     ],
   },
